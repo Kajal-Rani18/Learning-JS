bcc07f92cea4ce4744ded227a60deb8f007b0ceb
Kajal Rani <74965721+Kajal-Rani18@users.noreply.github.com>
2025-05-10 18:36:24 +0000

    variables in JS

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 2345
+let accountEmail = "kajal@google.com"
+var accountPassword = "12345"
+accountCity = "Meerut"
+let accountState;
+//accountId = 23198
+
+/*
+we will not be using var keyword 
+because of issue in block scope and functional scope
+*/
+
+
+accountEmail = 'kajal@mircosoft.com'
+accountPassword = "56207"
+accountCity = "Noida"
+console.log([accountId, accountEmail, accountPassword, accountCity]);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
